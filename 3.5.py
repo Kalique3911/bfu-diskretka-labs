4109a12caa6739fcc180dde73b8c175c4434d5a2
Kalique <kalique@gmail.com>
2025-04-09 15:30:55 +0200

    корректировка

--- 3.5.py
+++ 3.5.py
@@ -1,3 +1,9 @@
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 def arithmetic_encode(symbols, probabilities, string):
     low = 0.0
     high = 1.0
